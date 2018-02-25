52937b418fa7692c3a0022b56b004a7f3bcd7ca6
TheMesoria <freedomandjustit@gmail.com>
2018-02-25 21:15:05 +0100

    save

--- JavaLab/src/java/questionnaire/QuestionnaireCollection.java
+++ JavaLab/src/java/questionnaire/QuestionnaireCollection.java
@@ -4,20 +4,44 @@ import java.util.LinkedList;
 
 public class QuestionnaireCollection
 {
-    float failGrade;
-    float[] grading;
+	float failGrade;
+	float[] grading;
+	Questionnaire correctAnswers;
+	LinkedList<Questionnaire> questionnairesCollection;
 
-    Questionnaire correctAnswers;
-    LinkedList<Questionnaire> questionnairesCollection;
-
-    public QuestionnaireCollection(float failGrade, float[] grading, Questionnaire correctAnswers)
-    {
-        this.failGrade=failGrade;
-        this.grading=grading;
-        this.correctAnswers=correctAnswers;
-    }
-    public void addQuestionnaire(Questionnaire questionnaire)
-    {
-        
-    }
+	public QuestionnaireCollection(float failGrade, float[] grading, Questionnaire correctAnswers)
+	{
+		this.failGrade = failGrade;
+		this.grading = grading;
+		this.correctAnswers = correctAnswers;
+	}
+	
+	public void addQuestionnaire(Questionnaire questionnaire)
+	{
+		try
+		{
+			this.correctAnswers.compare(questionnaire);
+		}
+		catch (QuestionnaireMismatchException qme)
+		{
+			return;
+		}
+		this.questionnairesCollection.add(questionnaire);
+	}
+	public float getFailGrade()
+	{
+		return this.failGrade;
+	}
+	public float[] getGrading()
+	{
+		return this.grading;
+	}
+	public Questionnaire getCorrectAnswers()
+	{
+		return this.correctAnswers;
+	}
+	public LinkedList<Questionnaire> getQuestionnairesCollection()
+	{
+		return this.questionnairesCollection;
+	}
 }
