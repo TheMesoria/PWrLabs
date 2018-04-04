8054caaf0672c8112a7a8c265eb77fef7c6e3dd5
Black <freedomandjustit@gmail.com>
2018-04-04 10:46:38 +0200

    save

--- Lab5Task1V3/jarB/s.sh
+++ Lab5Task1V3/jarB/s.sh
@@ -1,8 +1,8 @@
-rm *.jar
+rm jar/*.jar
 rm ~/Work/Labs/Lab5Task1V3/jarB/jar/CalendarBean.fxml
 rm ~/Work/Labs/Lab5Task1V3/jarB/jar/calendarBean/CalendarBean.class 
-cp /home/black/Work/Labs/Lab5Task1V3/build/classes/java/main/calendarBean/* ~/Work/Labs/Lab5Task1V3/jarB/jar/calendarBean/
-cp /home/black/Work/Labs/Lab5Task1V3/build/resources/main/* ~/Work/Labs/Lab5Task1V3/jarB/jar/
+cp /home/black/Work/Labs/Lab5Task1V3/out/production/classes/calendarBean/* ~/Work/Labs/Lab5Task1V3/jarB/jar/calendarBean/
+cp /home/black/Work/Labs/Lab5Task1V3/out/production/resources/* ~/Work/Labs/Lab5Task1V3/jarB/jar/
 cd jar
 jar cvf JavaBean.jar .
 cd ..
