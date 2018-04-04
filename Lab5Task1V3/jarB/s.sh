5ed2e910fedd3568eb0528496cc92b4c4eef84be
Black <freedomandjustit@gmail.com>
2018-04-04 11:24:21 +0200

    seems like working

--- Lab5Task1V3/jarB/s.sh
+++ Lab5Task1V3/jarB/s.sh
@@ -4,5 +4,5 @@ rm ~/Work/Labs/Lab5Task1V3/jarB/jar/calendarBean/CalendarBean.class
 cp /home/black/Work/Labs/Lab5Task1V3/out/production/classes/calendarBean/* ~/Work/Labs/Lab5Task1V3/jarB/jar/calendarBean/
 cp /home/black/Work/Labs/Lab5Task1V3/out/production/resources/* ~/Work/Labs/Lab5Task1V3/jarB/jar/
 cd jar
-jar cvf JavaBean.jar .
+jar cvf calendarBean.jar .
 cd ..
