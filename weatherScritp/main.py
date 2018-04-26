d64e921902fe10c8847c54a92c89c2c9639f550d
Black <freedomandjustit@gmail.com>
2018-04-26 15:55:45 +0200

    Works

--- weatherScritp/main.py
+++ weatherScritp/main.py
@@ -3,35 +3,43 @@ import math
 
 
 def calc_partial_steam_pressure(temperature):
-    return 6.112 * math.exp((17.67*(temperature-273.15))/(temperature-273.15+243.5))
+    return 6.112 * math.exp(
+        (17.67 *
+         (float(temperature) - 273.15))
+        / (float(temperature) - 273.15 + 243.5))
 
 
 def calc_e(relative_humidity, partial_steam_pressure):
-    return relative_humidity * partial_steam_pressure / 100
+    return float(relative_humidity) * float(partial_steam_pressure) / 100
 
 
 def calc_ztd(partial_steam_steam_pressure, atmospheric_pressure, temperature):
-    return 0.002277 * (atmospheric_pressure + (1255 / temperature) + 0.05) \
-           * partial_steam_steam_pressure
+    return 0.002277 \
+           * (float(atmospheric_pressure) + (1255 / float(temperature)) + 0.05) \
+           * float(partial_steam_steam_pressure)
 
 
 def calc_g(geo_wdth, height, height_wrf):
-    return 9.8063 * (1 - (pow(10, -7) * (height_wrf+height) / 2)
+    return float(9.8063) * (1 - (pow(10, -7) * (float(height_wrf)+float(height)) / 2)
                      * 1 - 0.0026373
-                     * math.cos(2 * geo_wdth)
+                     * math.cos(2 * float(geo_wdth))
                      + 5.9
                      * pow(10, -6)
-                     * pow(
-                     math.cos(2 * geo_wdth), 2)
-                     )
+                     * pow(math.cos(2 * float(geo_wdth)), 2))
 
 
 def calc_p_when_not_given(height, pressure, temperature, height_wrf, g):
-    return pressure / (pow(((temperature - 0.0065*(height-height_wrf)) / temperature),
-                          (g * 0.0289644) / (8.31432 * 0.0065)))
+    return float(pressure) \
+           / (
+               pow(
+                   ((float(temperature) - 0.0065 * (float(height) - float(height_wrf)))
+                    / float(temperature)),
+                   (g * 0.0289644) / (8.31432 * 0.0065))
+           )
 
 
 csvData = []
+csvResults = []
 with open("daneLab02/surface_20150912_00.csv") as csvFile:
     reader = csv.reader(csvFile, delimiter=',', quotechar='|')
     for row in reader:
@@ -62,6 +70,6 @@ for row in csvData:
             calc_partial_steam_pressure(row[10])
         )
     )
-    print(out)
+    csvResults.append(out)
 
-print(calc_partial_steam_pressure(10))
+print(csvResults)
